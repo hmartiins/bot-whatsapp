8ee3e4dbdefe15c8eb80afe2613f31df3902812c
Henrique Martins <hmartins224@gmail.com>
2020-05-18 23:23:54 -0300

    Changing some details in the text

--- src/controllers/texts/index.js
+++ src/controllers/texts/index.js
@@ -1,13 +1,13 @@
 const text1 = (`
   Olá meu nome é Henrique Martins, sou estudante de desenvolvimento de sistemas
   na Etec de Peruíbe. Atualmente você está conversando com um bot que eu
-  programei hahah, mas calma ai, deixei umas opções de falas com o bot bem 
-  bacaninhas para vc testar, taca lhe pau ent. 
+  programei hahaha, mas calma ai, deixei umas opções de falas com o bot bem 
+  bacaninhas para vpcê testar, manda bala então ! 
   \n\n
-  [.1] * Com o que esse bot foi desenvolvido ?
+  [.1] * Como esse bot foi desenvolvido ?
 `);
 const text2 = (`
-  Bom vamos lá, a primeira versão desse bot n tem nenhuma interface gráfica
+  Bom vamos lá, a primeira versão desse bot não tem nenhuma interface gráfica
   nem nada, apenas o terminal, então estou utilizando apenas Node.JS, e para 
   capturar e enviar as mensagens, uma lib bem bacana do whatsapp para o Node.
 `)
